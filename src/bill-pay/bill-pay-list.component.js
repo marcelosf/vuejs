c133c962e59e5868156cde6d90f009df9bd31055
Marcelo Schneider <schneider.fei@gmail.com>
2017-01-02 15:06:38 +0000

    Modals

--- src/bill-pay/bill-pay-list.component.js
+++ src/bill-pay/bill-pay-list.component.js
@@ -4,59 +4,54 @@ window.billPayListComponent = Vue.extend({
                     <div class="container">
                     
                         <div class="row">
-                        <div>
-                             <button class="btn btn-large waves-effect">Meu botão</button>
-                             <h2>Minhas Contas a Pagar</h2>
-                             <table class="striped centered highlight z-depth-3" cellpadding="10">
+                            <div>
+                                 <button class="btn btn-large waves-effect">Meu botão</button>
+                                 <h2>Minhas Contas a Pagar</h2>
+                                 <table class="striped centered highlight z-depth-3" cellpadding="10">
                                        <thead>
-                                       <tr>
-                                           <td>#</td>
-                                           <th>Vencimento</th>
-                                           <th>Nome</th>
-                                           <th>Valor</th>
-                                           <th>Paga?</th>
-                                           <th>Ações</th>
-                                       </tr>
-                                       </thead>
-                    
-                                       <tbody>
-                                       <tr v-for="(index,o) in bills">
-                                           <td>{{ index + 1 }}</td>
-                                           <td>{{ o.date_due | dateFormat 'en-US' }}</td>
-                                           <td>{{ o.name }}</td>
-                                           <td>{{ o.value | numberFormat 'en-US' 'USD' }}</td>
-                                           <td class="white-text" :class="{'green lighten-2': o.done, 'red lighten-2': !o.done}">
-                                               {{ o.done | doneLabel }}
-                                           </td>
-                                           <td>
-                                               <a v-link="{name: 'bill-pay.update', params: {id: o.id }}">Editar|</a>
-                                               <a href="#" @click.prevent="$parent.removeBill(o)">Remover</a>
-                                               <!--<a href="#" @click.prevent="$parent.baixa(o)">{{ o.done | paidLabel }}</a>-->
-                                           </td>
-                                       </tr>
-                                       </tbody>
-                                   </table>
-                        </div>
-                           
-                        </div>
-                        </div>
+                                           <tr>
+                                               <td>#</td>
+                                               <th>Vencimento</th>
+                                               <th>Nome</th>
+                                               <th>Valor</th>
+                                               <th>Paga?</th>
+                                               <th>Ações</th>
+                                           </tr>
+                                           </thead>
                         
-                        <div>
-                            <a id="btn-modal" href="#my-modal" class="btn waves-effect">Abrir Modal</a>
-                            <div id="my-modal" class="modal">
-                                <div class="modal-content">
-                                    <h2>Modal Content</h2>
-                                    <p>Content text</p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button class="btn btn-flat green">Ok</button>
-                                </div>
-                            
+                                           <tbody>
+                                           <tr v-for="(index,o) in bills">
+                                               <td>{{ index + 1 }}</td>
+                                               <td>{{ o.date_due | dateFormat 'en-US' }}</td>
+                                               <td>{{ o.name }}</td>
+                                               <td>{{ o.value | numberFormat 'en-US' 'USD' }}</td>
+                                               <td class="white-text" :class="{'green lighten-2': o.done, 'red lighten-2': !o.done}">
+                                                   {{ o.done | doneLabel }}
+                                               </td>
+                                               <td>
+                                                   <a v-link="{name: 'bill-pay.update', params: {id: o.id }}">Editar|</a>
+                                                   <a href="#" @click.prevent="$parent.removeBill(o)">Remover</a>
+                                                   <!--<a href="#" @click.prevent="$parent.baixa(o)">{{ o.done | paidLabel }}</a>-->
+                                               </td>
+                                           </tr>
+                                      </tbody>
+                                 </table>
                             </div>
-                        
                         </div>
+                    </div>
                        
-    
+                    <a id="btn-modal" href="#meu-modal" class="btn waves-effect">Abrir Modal</a>
+                    <button data-target="meu-modal" class="btn waves-effect">Abrir Modal</button>
+                       
+                    <div id="meu-modal" class="modal">
+                        <div class="modal-content">
+                            <h2>Meu Primeiro Modal</h2>
+                            <p>Texto .....</p>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-flat green modal-action modal-close">Ok</button>
+                        </div>
+                    </div>
     `,
 
     data() {
@@ -66,13 +61,17 @@ window.billPayListComponent = Vue.extend({
     },
 
     created() {
+        $(document).ready(function(){
+
+            $('#meu-modal').modal();
+
+        });
 
         Bill.query().then((response) => {
             this.bills = response.data;
         });
-        $(document).ready(function () {
-            $("#btn-modal").modal();
-        });
+
+
     } ,
 
 
